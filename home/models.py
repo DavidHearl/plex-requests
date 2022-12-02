87fd6755ff3b38b738fe9884d55f4fbf8c230ebd
DavidHearl <david.hearl@outlook.com>
2022-12-02 14:24:08 +0000

    added model for movies

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Movies(models.Model):
+    category = models.ForeignKey('Category', null=False, blank=False, on_delete=models.CASCADE)
+    title = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
